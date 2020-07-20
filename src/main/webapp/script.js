269e35dc5d35b1d88fd95e022ce9acaf1dc2d365
Sofia Fernandez Chacin <sofiafch2000@gmail.com>
2020-07-20 11:50:53 -0500

    Generate overlay to enlarge an initiative.

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -15,6 +15,7 @@
 /**
  * Adds a random greeting to the page.
  */
+var cardCount = 0;
 
 function getMessage() {
     getBlobstoreUrl();
@@ -26,9 +27,11 @@ function getMessage() {
         var startDatesList = fullList[2];
         var urlsList = fullList[8];
         for (i = 0; i < titlesList.length; i++) {
-            titlesListElement.appendChild(createDivElement(titlesList[i], startDatesList[i],
+            cardCount++;
+            titlesListElement.appendChild(createDivElement(cardCount, titlesList[i], startDatesList[i],
              contextsList[i], urlsList[i]));
         }
+        cardEnlargement(cardCount);
     });
 }
 
@@ -42,10 +45,13 @@ function getBlobstoreUrl() {
         myForm.classList.remove('hidden');
       });
 }
-console.log("This is the blobstore url"+getBlobstoreUrl() );
 
-function createDivElement(initiativeTitle, initiativeStart, initiativeContext, initiativeImage) {
+console.log(getBlobstoreUrl());
+
+function createDivElement(cardCount,initiativeTitle, initiativeStart,
+ initiativeContext, initiativeImage) {
   const currentCard = document.createElement('div');
+  currentCard.id = "card" + cardCount;
   const currStartDate = document.createElement('p');
   const currTitle = document.createElement('p');
   const currContext = document.createElement('p');
@@ -57,18 +63,22 @@ function createDivElement(initiativeTitle, initiativeStart, initiativeContext, i
   currImage.style.marginTop = "25px";
   currImage.style.marginLeft = "15px";
   currImage.style.marginRight = "13px";
+  currImage.id = "the-image" + "card" + cardCount;
   currTitle.innerText = initiativeTitle;
   currTitle.style.fontFamily = "'Roboto', sans-serif";
   currTitle.style.fontWeight = "bolder";
   currTitle.style.textTransform = "uppercase";
   currTitle.style.fontSize = "90%";
+  currTitle.id = "the-title" + "card" + cardCount;
   currStartDate.innerText = initiativeStart;
   currStartDate.style.fontFamily = "'Roboto', sans-serif";
   currStartDate.style.fontSize = "80%";
+  currStartDate.id = "the-start" + "card" + cardCount;
   currContext.innerText = initiativeContext;
   currContext.style.fontFamily = "'Roboto', sans-serif";
   currContext.style.fontSize = "80%";
   currContext.style.marginRight = "12px";
+  currContext.id = "the-context" + "card" + cardCount;
   currentCard.style.backgroundColor = "#E5E5E5";
   currentCard.style.width = "300px";
   currentCard.style.height = "130px";
@@ -80,4 +90,55 @@ function createDivElement(initiativeTitle, initiativeStart, initiativeContext, i
   currentCard.appendChild(currStartDate);
   currentCard.appendChild(currContext);
   return currentCard;
+}
+
+function cardEnlargement(cardCount) {
+    console.log(cardCount);
+    for (j = 1; j <= cardCount; j++) {
+        console.log("card-enlargement-call");
+        var currCardEvent = document.getElementById("card" + j);
+        currCardEvent.addEventListener("mouseover", myHoverFunction);
+        currCardEvent.addEventListener("click", myClickFunction);
+        currCardEvent.addEventListener("mouseout", myMouseOutFunction);
+    }
+}
+
+function myHoverFunction() {
+    this.style.border = "2px solid black";
+}
+
+function myMouseOutFunction() {
+    this.style.border = "0px";
+}
+
+function myClickFunction() {
+    var theTitle = document.getElementById('the-title'+ this.id);
+    var theImage = document.getElementById('the-image' + this.id);
+    var theStart = document.getElementById('the-start' + this.id);
+    var theContext = document.getElementById('the-context' + this.id);
+    var cloneTitle = theTitle.cloneNode(theTitle);
+    var cloneImage = theImage.cloneNode(theImage);
+    var cloneStart = theStart.cloneNode(theStart);
+    var cloneContext = theContext.cloneNode(theContext);
+    const titlePlaceholder = document.getElementById('initiative-overlay-title');
+    const imagePlaceholder = document.getElementById('initiative-overlay-image');
+    const startPlaceholder = document.getElementById('initiative-overlay-start-date');
+    const contextPlaceholder = document.getElementById('initiative-overlay-context');
+    titlePlaceholder.innerHTML = '';
+    imagePlaceholder.innerHTML = '';
+    startPlaceholder.innerHTML = '';
+    contextPlaceholder.innerHTML = '';
+    titlePlaceholder.appendChild(cloneTitle);
+    imagePlaceholder.appendChild(cloneImage);
+    startPlaceholder.appendChild(cloneStart);
+    contextPlaceholder.appendChild(cloneContext);
+    open();
+}
+
+function open() {
+    document.getElementById('initiative-overlay').style.width = "100%";
+}
+
+function close() {
+    document.getElementById('initiative-overlay').style.width = "0%";
 }
