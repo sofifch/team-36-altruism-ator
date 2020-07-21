487fbcce7c5421226f8ec437b3485328277e310d
Sofia Fernandez Chacin <sofiafch2000@gmail.com>
2020-07-21 17:08:37 -0500

    Fix details on userServlet and add js file for profile view.

---
+++ src/main/webapp/profileScript.js
@@ -0,0 +1,8 @@
+function updateVolunteeringStatus() {
+    var checkbox = document.getElementById('myCheck');
+    if (checkbox.checked == true) {
+        console.log("checked");
+    } else {
+        console.log("not checked");
+    }
+}
