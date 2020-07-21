3f73f1f7692c11e5f3633b4a4aa8956cded9ce42
Sofia Fernandez Chacin <sofiafch2000@gmail.com>
2020-07-20 20:38:51 -0500

    Finish card enlargement feature.

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -26,10 +26,15 @@ function getMessage() {
         var contextsList = fullList[4];
         var startDatesList = fullList[2];
         var urlsList = fullList[8];
+        var locationsList = fullList[1];
+        var causesList = fullList[5];
+        var audiencesList = fullList[6];
+        var instructionsList = fullList[7];
         for (i = 0; i < titlesList.length; i++) {
             cardCount++;
             titlesListElement.appendChild(createDivElement(cardCount, titlesList[i], startDatesList[i],
-             contextsList[i], urlsList[i]));
+             contextsList[i], urlsList[i], locationsList[i], causesList[i], audiencesList[i],
+             instructionsList[i]));
         }
         cardEnlargement(cardCount);
     });
@@ -49,13 +54,30 @@ function getBlobstoreUrl() {
 console.log(getBlobstoreUrl());
 
 function createDivElement(cardCount,initiativeTitle, initiativeStart,
- initiativeContext, initiativeImage) {
+ initiativeContext, initiativeImage, initiativeLocation, initiativeCause, initiativeAudience, 
+ initiativeInstructions) {
   const currentCard = document.createElement('div');
   currentCard.id = "card" + cardCount;
   const currStartDate = document.createElement('p');
   const currTitle = document.createElement('p');
   const currContext = document.createElement('p');
   const currImage = document.createElement('img');
+  const currLocation = document.createElement('p');
+  const currCause = document.createElement('p');
+  const currAudience = document.createElement('p');
+  const currInstructions = document.createElement('p');
+  currLocation.innerText = initiativeLocation;
+  currLocation.id = "the-location" + "card" + cardCount;
+  currLocation.style.display = "none";
+  currAudience.innerText = initiativeAudience;
+  currAudience.id = "the-audience" + "card" + cardCount;
+  currAudience.style.display = "none";
+  currInstructions.innerText = initiativeInstructions;
+  currInstructions.id = "the-instructions" + "card" + cardCount;
+  currInstructions.style.display = "none";
+  currCause.innerText = initiativeCause;
+  currCause.id = "the-cause" + "card" + cardCount;
+  currCause.style.display = "none";
   currImage.src = initiativeImage;
   currImage.style.width = "85px";
   currImage.style.height = "80px";
@@ -89,6 +111,10 @@ function createDivElement(cardCount,initiativeTitle, initiativeStart,
   currentCard.appendChild(currTitle);
   currentCard.appendChild(currStartDate);
   currentCard.appendChild(currContext);
+  currentCard.appendChild(currLocation);
+  currentCard.appendChild(currCause);
+  currentCard.appendChild(currAudience);
+  currentCard.appendChild(currInstructions);
   return currentCard;
 }
 
@@ -116,22 +142,48 @@ function myClickFunction() {
     var theImage = document.getElementById('the-image' + this.id);
     var theStart = document.getElementById('the-start' + this.id);
     var theContext = document.getElementById('the-context' + this.id);
+    var theLocation = document.getElementById('the-location' + this.id);
+    var theCause = document.getElementById('the-cause' + this.id);
+    var theAudience = document.getElementById('the-audience' + this.id);
+    var theInstructions = document.getElementById('the-instructions' + this.id);
     var cloneTitle = theTitle.cloneNode(theTitle);
     var cloneImage = theImage.cloneNode(theImage);
-    var cloneStart = theStart.cloneNode(theStart);
-    var cloneContext = theContext.cloneNode(theContext);
+    cloneImage.style.width = "250px";
+    cloneImage.style.height = "250px";
+    var cloneStart = theStart.cloneNode(true);
+    var cloneContext = theContext.cloneNode(true);
+    var cloneLocation = theLocation.cloneNode(true);
+    var cloneCause = theCause.cloneNode(true);
+    var cloneAudience = theAudience.cloneNode(true);
+    var cloneInstructions = theInstructions.cloneNode(true);
+    cloneLocation.style.display = "inline";
+    cloneCause.style.display = "inline";
+    cloneAudience.style.display = "inline";
+    cloneInstructions.style.display = "inline";
     const titlePlaceholder = document.getElementById('initiative-overlay-title');
     const imagePlaceholder = document.getElementById('initiative-overlay-image');
     const startPlaceholder = document.getElementById('initiative-overlay-start-date');
     const contextPlaceholder = document.getElementById('initiative-overlay-context');
+    const locationPlaceholder = document.getElementById('initiative-overlay-location');
+    const causePlaceholder = document.getElementById('initiative-overlay-cause');
+    const audiencePlaceholder = document.getElementById('initiative-overlay-audience');
+    const instructionsPlaceholder = document.getElementById('initiative-overlay-instructions');
     titlePlaceholder.innerHTML = '';
     imagePlaceholder.innerHTML = '';
     startPlaceholder.innerHTML = '';
     contextPlaceholder.innerHTML = '';
+    locationPlaceholder.innerHTML = '';
+    causePlaceholder.innerHTML = '';
+    audiencePlaceholder.innerHTML = '';
+    instructionsPlaceholder.innerHTML = '';
     titlePlaceholder.appendChild(cloneTitle);
     imagePlaceholder.appendChild(cloneImage);
     startPlaceholder.appendChild(cloneStart);
     contextPlaceholder.appendChild(cloneContext);
+    locationPlaceholder.appendChild(cloneLocation);
+    causePlaceholder.appendChild(cloneCause);
+    audiencePlaceholder.appendChild(cloneAudience);
+    instructionsPlaceholder.appendChild(cloneInstructions);
     open();
 }
 
