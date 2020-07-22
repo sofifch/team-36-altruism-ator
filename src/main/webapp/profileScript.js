e3f81ea32158c5a6baa888c386787bb78dc0086f
Sofia Fernandez Chacin <sofiafch2000@gmail.com>
2020-07-22 11:44:51 -0500

    Persist volunteering status through page loads/refreshes.

--- src/main/webapp/profileScript.js
+++ src/main/webapp/profileScript.js
@@ -9,12 +9,32 @@ function getCurrentUserInfo() {
         theUsername.innerText = userInfo.username;
         currentUsername.appendChild(theUsername);
     });
+    loadPrevStatus();
+}
+
+function loadPrevStatus() {
+    if (localStorage.getItem('myCheckbox') != null) {
+        if (localStorage.getItem('myCheckbox') == "checked") {
+            const lastVolunteeringStatus = document.getElementById('neutral-volunteering-status-btn');
+            lastVolunteeringStatus.style.display = "none";
+            const volunteeringStatus = document.getElementById('interested-volunteering-status-btn');
+            volunteeringStatus.style.display = "block";
+            const checkbox = document.getElementById('myCheck');
+            checkbox.checked = true;
+        } else {
+            const lastVolunteeringStatus = document.getElementById('interested-volunteering-status-btn');
+            lastVolunteeringStatus.style.display = "none";
+            const volunteeringStatus = document.getElementById('neutral-volunteering-status-btn');
+            volunteeringStatus.style.display = "block";
+        }
+    }
 }
 
 $(document).ready(function(){
         $('input[type="checkbox"]').click(function(){
             if($(this).prop("checked") == true){
                 console.log("Checkbox is checked.");
+                localStorage.setItem('myCheckbox', 'checked');
                 const lastVolunteeringStatus = document.getElementById('neutral-volunteering-status-btn');
                 lastVolunteeringStatus.style.display = "none";
                 const volunteeringStatus = document.getElementById('interested-volunteering-status-btn');
@@ -22,6 +42,7 @@ $(document).ready(function(){
             }
             else if($(this).prop("checked") == false){
                 console.log("Checkbox is unchecked.");
+                localStorage.setItem('myCheckbox', 'unchecked');
                 const lastVolunteeringStatus = document.getElementById('interested-volunteering-status-btn');
                 lastVolunteeringStatus.style.display = "none";
                 const volunteeringStatus = document.getElementById('neutral-volunteering-status-btn');
