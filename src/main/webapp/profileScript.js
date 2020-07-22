23dc169db7e277923d9dc2eccf7c354fd3d885b2
Sofia Fernandez Chacin <sofiafch2000@gmail.com>
2020-07-22 10:39:30 -0500

    Fetch name and username from servlet to display it.

--- src/main/webapp/profileScript.js
+++ src/main/webapp/profileScript.js
@@ -1,3 +1,16 @@
+function getCurrentUserInfo() {
+    fetch('/user').then(response => response.json()).then((userInfo) => {
+        const currentName = document.getElementById('current-name');
+        const theName = document.createElement('p');
+        theName.innerText = userInfo.name;
+        currentName.appendChild(theName);
+        const currentUsername = document.getElementById('current-username');
+        const theUsername = document.createElement('p');
+        theUsername.innerText = userInfo.username;
+        currentUsername.appendChild(theUsername);
+    });
+}
+
 function updateVolunteeringStatus() {
     var checkbox = document.getElementById('myCheck');
     if (checkbox.checked == true) {
